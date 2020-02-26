0045fef665d89633179f6e24f239dc98db5f8715
Amador Santiago <amadorsantiago368@gmail.com>
2020-02-26 00:54:56 -0600

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_26_062742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "image_main"
+    t.string "image_detail"
+    t.text "description"
+    t.decimal "price"
+    t.decimal "weight"
+    t.integer "reviews"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
