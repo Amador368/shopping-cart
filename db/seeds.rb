fd0312cfcc5638f0c20aadf7266599fabd651aee
Amador Santiago <amadorsantiago368@gmail.com>
2020-02-27 06:39:57 -0600

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,67 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(
+    name: "Missha All Around Safe Block Sebum Zero Sun",
+    image_main: "/assets/product01a.png",
+    image_detail: "/assets/product01b.png",
+    description: "A portable cooling eye stick that instantly relieves puffy, tired eyes by hydrating the delicate skin and minimizing fine lines around the eye for a refreshed and revitalized look.",
+    price: 4.00,
+    weight: 50,
+    reviews: 21)
+Product.create(
+    name: "Innisfree Green Tea Balancing Cream",
+    image_main: "/assets/product02a.png",
+    image_detail: "/assets/product02b.png",
+    description: "A portable cooling eye stick that instantly relieves puffy, tired eyes by hydrating the delicate skin and minimizing fine lines around the eye for a refreshed and revitalized look.",
+    price: 18.00,
+    weight: 52,
+    reviews: 11)
+Product.create(
+    name: "Claire’s Cloud 9 Blanc De Whitening Cream",
+    image_main: "/assets/product03a.png",
+    image_detail: "/assets/product03b.png",
+    description: "A portable cooling eye stick that instantly relieves puffy, tired eyes by hydrating the delicate skin and minimizing fine lines around the eye for a refreshed and revitalized look.",
+    price: 20.00,
+    weight: 60,
+    reviews: 27)
+Product.create(
+    name: "Tonymoly Pure Eco Snail Moisture Gel",
+    image_main: "/assets/product04a.png",
+    image_detail: "/assets/product04b.png",
+    description: "A portable cooling eye stick that instantly relieves puffy, tired eyes by hydrating the delicate skin and minimizing fine lines around the eye for a refreshed and revitalized look.",
+    price: 8.00,
+    weight: 45,
+    reviews: 19)
+Product.create(
+    name: "Skin79 Fresh Garden Mask - Snail",
+    image_main: "/assets/product05a.png",
+    image_detail: "/assets/product05b.png",
+    description: "A portable cooling eye stick that instantly relieves puffy, tired eyes by hydrating the delicate skin and minimizing fine lines around the eye for a refreshed and revitalized look.",
+    price: 2.00,
+    weight: 52,
+    reviews: 13)
+Product.create(
+    name: "Cosrx Advanced Snail 92 All in One Cream",
+    image_main: "/assets/product06a.png",
+    image_detail: "/assets/product06b.png",
+    description: "A portable cooling eye stick that instantly relieves puffy, tired eyes by hydrating the delicate skin and minimizing fine lines around the eye for a refreshed and revitalized look.",
+    price: 24.00,
+    weight: 35,
+    reviews: 8)
+Product.create(
+    name: "Innisfree Canola Honey Mask",
+    image_main: "/assets/product07a.png",
+    image_detail: "/assets/product07b.png",
+    description: "A portable cooling eye stick that instantly relieves puffy, tired eyes by hydrating the delicate skin and minimizing fine lines around the eye for a refreshed and revitalized look.",
+    price: 14.00,
+    weight: 40,
+    reviews: 16)
+Product.create(
+    name: "Scinic Honey Banana Cleansing Foam",
+    image_main: "/assets/product08a.png",
+    image_detail: "/assets/product08b.png",
+    description: "A portable cooling eye stick that instantly relieves puffy, tired eyes by hydrating the delicate skin and minimizing fine lines around the eye for a refreshed and revitalized look.",
+    price: 4.00,
+    weight: 55,
+    reviews: 12)    
